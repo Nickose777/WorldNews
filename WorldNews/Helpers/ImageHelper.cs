9d6fc80cb04e4b9342ce8755eb206dbc9a904107
Nickose777 <my_personal@mail.ua>
2017-07-22 01:44:23 +0300

    Moderator details page UI

---
+++ WorldNews/Helpers/ImageHelper.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace WorldNews.Helpers
+{
+    public static class ImageHelper
+    {
+        public static MvcHtmlString Image(this HtmlHelper helper, string src, string altText)
+        {
+            return Image(helper, src, altText, null);
+        }
+
+        public static MvcHtmlString Image(this HtmlHelper helper, string src, string altText, object htmlAttributes)
+        {
+            TagBuilder builder = new TagBuilder("img");
+            builder.MergeAttribute("src", src);
+            builder.MergeAttribute("alt", altText);
+
+            if (htmlAttributes != null)
+            {
+                RouteValueDictionary attributes = HtmlHelper.AnonymousObjectToHtmlAttributes(htmlAttributes);
+                foreach (var attribute in attributes)
+                {
+                    builder.Attributes.Add(attribute.Key, attribute.Value.ToString());
+                }
+            }
+
+            return MvcHtmlString.Create(builder.ToString(TagRenderMode.SelfClosing));
+        }
+    }
+}
--- WorldNews/Models/Profile/ModeratorDetailsViewModel.cs
+++ WorldNews/Models/Profile/ModeratorDetailsViewModel.cs
@@ -1,18 +1,23 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using WorldNews.Models.Comment;
 
 namespace WorldNews.Models.Profile
 {
     public class ModeratorDetailsViewModel
     {
+        [Display(Name = "First name:")]
         public string FirstName { get; set; }
 
+        [Display(Name = "Last name:")]
         public string LastName { get; set; }
 
         public string PhotoLink { get; set; }
 
+        [Display(Name = "Login:")]
         public string Login { get; set; }
 
+        [Display(Name = "E-Mail:")]
         public string Email { get; set; }
 
         public IEnumerable<CommentBanDetailsViewModel> BannedComments { get; set; }
